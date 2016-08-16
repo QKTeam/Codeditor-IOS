692a4f73d1108c2db63eb819a2c20a3b3c58c066
GuessEver <guessever@gmail.com>
2016-08-16 20:33:20 +0800

    language regrex updated

--- Codeditor/CodeditorLanguagePattern.m
+++ Codeditor/CodeditorLanguagePattern.m
@@ -61,19 +61,21 @@
     switch (type) {
         case CodeditorLanguageC:
             return [self initWithLanguage:CodeditorLanguagePascal
-                                  comment:@[ [[CodeditorPattern alloc] initWithPattern:@"//(.*)$" leftOffset:0 rightOffset:0],
-                                             [[CodeditorPattern alloc] initWithPattern:@"/\\*(.*)\\*/" leftOffset:0 rightOffset:0]]
+                                  comment:@[
+                                            [[CodeditorPattern alloc] initWithPattern:@"//(.*)$" leftOffset:0 rightOffset:0],
+                                            [[CodeditorPattern alloc] initWithPattern:@"/\\*(.*?)\\*/" leftOffset:0 rightOffset:0]]
                                   grammar:@[
                                             [[CodeditorPattern alloc]initWithPattern:@"\\b(void|bool|char|float|int|double|short|long|unsigned|signed|struct|union|enum|typedef|sizeof|auto|static|register|extern|const|volatile|return|continue|break|goto|if|else|switch|case|default|for|do|while|include)\\b" leftOffset:0 rightOffset:0]
                                             ]
                                   keyword:@[
-                                            [[CodeditorPattern alloc] initWithPattern:@"\\b([a-zA-Z0-9_]+?)\\(" leftOffset:0 rightOffset:1]]
+                                            [[CodeditorPattern alloc] initWithPattern:@"\\b([a-zA-Z0-9_]+?)\\(" leftOffset:0 rightOffset:1]
+                                            ]
                     ];
         case CodeditorLanguageCpp:
             return [self initWithLanguage:CodeditorLanguageCpp
                                   comment:@[
                                             [[CodeditorPattern alloc] initWithPattern:@"//(.*)$" leftOffset:0 rightOffset:0],
-                                            [[CodeditorPattern alloc] initWithPattern:@"/\\*(.*)\\*/" leftOffset:0 rightOffset:0]
+                                            [[CodeditorPattern alloc] initWithPattern:@"/\\*(.*?)\\*/" leftOffset:0 rightOffset:0]
                                             ]
                                   grammar:@[
                                             [[CodeditorPattern alloc] initWithPattern:@"\\b(asm|auto|bool|break|case|catch|char|class|const|const_cast|continue|default|delete|do|double|dynamic_cast|else|enum|explicit|export|extern|false|float|for|friend|goto|if|inline|int|long|mutable|namespace|new|operator|private|protected |public|register|reinterpret_cast|return|short|signed|sizeof|static|static_cast|struct|switch|template|this|throw|true|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|while)\\b" leftOffset:0 rightOffset:0]
@@ -85,29 +87,37 @@
             
         case CodeditorLanguagePascal:
             return [self initWithLanguage:CodeditorLanguagePascal
-                                  comment:@[]
+                                  comment:@[
+                                            [[CodeditorPattern alloc] initWithPattern:@"//(.*)$" leftOffset:0 rightOffset:0],
+                                            [[CodeditorPattern alloc] initWithPattern:@"\\{(.*?)\\}" leftOffset:0 rightOffset:0]
+                                            ]
                                   grammar:@[
                                             [[CodeditorPattern alloc ] initWithPattern:@"\\b(absolute|abstract|and|array|as|asm|assembler|at|automated|begin|case|cdecl|class|const|constructor|contains|default|destructor|dispid|dispinterface|div|do|downto|dynamic|else|end|except|export|exports|external|far|file|finalization|finally|for|forward|function|goto|if|implementation|implements|in|index|inherited|initialization|inline|interface|is|label|library|message|mod|name|near|nil|nodefault|not|object|of|on|or|out|overload|override|package|packed|pascal|private|procedure|program|property|protected|public|published|raise|read|readonly|record|register|reintroduce|repeat|requires|resident|resourcestring|safecall|set|shl|shr|stdcall|stored|string|then|threadvar|to|try|type|unit|until|uses|var|virtual|while|with|write|writeonly|xor)\\b" leftOffset:0 rightOffset:0]
                                             ]
                                   keyword:@[
-                                            [[CodeditorPattern alloc] initWithPattern:@"\\b([a-zA-Z0-9_]+?)\\(" leftOffset:0 rightOffset:1]]
+                                            [[CodeditorPattern alloc] initWithPattern:@"\\b([a-zA-Z0-9_]+?)\\(" leftOffset:0 rightOffset:1]
+                                            ]
                     ];
             
         case CodeditorLanguageJava:
             return [self initWithLanguage:CodeditorLanguageJava
-                                  comment:@[]
+                                  comment:@[
+                                            [[CodeditorPattern alloc] initWithPattern:@"\\{(.*?)\\}" leftOffset:0 rightOffset:0]
+                                            ]
                                   grammar:@[
                                             [[CodeditorPattern alloc]initWithPattern:@"\\b(abstract|boolean|break|byte|case|catch|char|class|continue|default|do|double|else|extends|false|final|finally|float|for|if|implements|import|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|super|seitch|synchronized|this|throw|throws|transient|try|true|void|volatile|while)\\b" leftOffset:0 rightOffset:0]
                                             ]
                                   keyword:@[
-                                            [[CodeditorPattern alloc] initWithPattern:@"\\b([a-zA-Z0-9_]+?)\\(" leftOffset:0 rightOffset:1]]
+                                            [[CodeditorPattern alloc] initWithPattern:@"\\b([a-zA-Z0-9_]+?)\\(" leftOffset:0 rightOffset:1]
+                                            ]
                     ];
             
         default:
             return [self initWithLanguage:CodeditorLanguagePlain
                                   comment:@[]
                                   grammar:@[]
-                                  keyword:@[]];
+                                  keyword:@[]
+                    ];
     }
 }
 
