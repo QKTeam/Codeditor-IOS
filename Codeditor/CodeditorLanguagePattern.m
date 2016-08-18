ea6430400b6e7d415d0cd621d68d5f30875320c1
GuessEver <guessever@gmail.com>
2016-08-18 11:10:24 +0800

    fix comment pattern expression

--- Codeditor/CodeditorLanguagePattern.m
+++ Codeditor/CodeditorLanguagePattern.m
@@ -104,10 +104,10 @@
                                     [CodeditorPattern initWithPattern:@"\\b[0-9]+\\b"]
                                     ]
                         character:@[
-                                    [CodeditorPattern initWithPattern:@"'(.*?)'"]
+                                    [CodeditorPattern initWithPattern:@"'(.*?)('|$)"]
                                     ]
                            string:@[
-                                    [CodeditorPattern initWithPattern:@"\"(.*?)\""]
+                                    [CodeditorPattern initWithPattern:@"\"(.*?)(\"|$)"]
                                     ]
                           grammar:grammar
                           keyword:keyword
@@ -117,13 +117,12 @@
 + (instancetype)initWithLanguage:(CodeditorLanguageType)type {
     switch (type) {
         case CodeditorLanguageC:
-            return [self initWithLanguage:CodeditorLanguagePascal
+            return [self initWithLanguage:CodeditorLanguageC
                      codeBlockBeginSymbol:@"{"
                        codeBlockEndSymbol:@"}"
                        withPatternComment:@[
                                             [CodeditorPattern initWithPattern:@"//(.*)"],
-                                            [CodeditorPattern initWithPattern:@"/\\*(.*?)\\*/" globalMatch:YES],
-                                            [CodeditorPattern initWithPattern:@"/\\*((?!\\*/).)*" globalMatch:YES]
+                                            [CodeditorPattern initWithPattern:@"/\\*(.*?)(\\*/|$)" globalMatch:YES]
                                             ]
                                   grammar:@[
                                             [CodeditorPattern initWithPattern:@"\\b(void|bool|char|float|int|double|short|long|unsigned|signed|struct|union|enum|typedef|sizeof|auto|static|register|extern|const|volatile|return|continue|break|goto|if|else|switch|case|default|for|do|while|include)\\b"]
@@ -139,28 +138,10 @@
                        codeBlockEndSymbol:@"}"
                        withPatternComment:@[
                                             [CodeditorPattern initWithPattern:@"//(.*)"],
-                                            [CodeditorPattern initWithPattern:@"/\\*(.*?)\\*/" globalMatch:YES],
-                                            [CodeditorPattern initWithPattern:@"/\\*((?!\\*/).)*" globalMatch:YES]
+                                            [CodeditorPattern initWithPattern:@"/\\*(.*?)(\\*/|$)" globalMatch:YES]
                                             ]
                                   grammar:@[
                                             [CodeditorPattern initWithPattern:@"\\b(asm|auto|bool|break|case|catch|char|class|const|const_cast|continue|default|delete|do|double|dynamic_cast|else|enum|explicit|export|extern|false|float|for|friend|goto|if|inline|int|long|mutable|namespace|new|operator|private|protected |public|register|reinterpret_cast|return|short|signed|sizeof|static|static_cast|struct|switch|template|this|throw|true|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|while)\\b"]
-                                            ]
-                                  keyword:@[
-                                            [CodeditorPattern initWithPattern:@"\\b([a-zA-Z0-9_]+?)\\(" rightOffset:1]
-                                            ]
-                    ];
-            
-        case CodeditorLanguagePascal:
-            return [self initWithLanguage:CodeditorLanguagePascal
-                     codeBlockBeginSymbol:@"begin"
-                       codeBlockEndSymbol:@"end"
-                       withPatternComment:@[
-                                            [CodeditorPattern initWithPattern:@"//(.*)"],
-                                            [CodeditorPattern initWithPattern:@"\\{(.*?)\\}" globalMatch:YES],
-                                            [CodeditorPattern initWithPattern:@"\\{((?!\\}).)*" globalMatch:YES]
-                                            ]
-                                  grammar:@[
-                                            [CodeditorPattern initWithPattern:@"\\b(absolute|abstract|and|array|as|asm|assembler|at|automated|begin|case|cdecl|class|const|constructor|contains|default|destructor|dispid|dispinterface|div|do|downto|dynamic|else|end|except|export|exports|external|far|file|finalization|finally|for|forward|function|goto|if|implementation|implements|in|index|inherited|initialization|inline|interface|is|label|library|message|mod|name|near|nil|nodefault|not|object|of|on|or|out|overload|override|package|packed|pascal|private|procedure|program|property|protected|public|published|raise|read|readonly|record|register|reintroduce|repeat|requires|resident|resourcestring|safecall|set|shl|shr|stdcall|stored|string|then|threadvar|to|try|type|unit|until|uses|var|virtual|while|with|write|writeonly|xor)\\b"]
                                             ]
                                   keyword:@[
                                             [CodeditorPattern initWithPattern:@"\\b([a-zA-Z0-9_]+?)\\(" rightOffset:1]
@@ -173,11 +154,26 @@
                        codeBlockEndSymbol:@"}"
                        withPatternComment:@[
                                             [CodeditorPattern initWithPattern:@"//(.*)"],
-                                            [CodeditorPattern initWithPattern:@"/\\*(.*?)\\*/" globalMatch:YES],
-                                            [CodeditorPattern initWithPattern:@"/\\*((?!\\*/).)*" globalMatch:YES]
+                                            [CodeditorPattern initWithPattern:@"/\\*(.*?)(\\*/|$)" globalMatch:YES]
                                             ]
                                   grammar:@[
                                             [CodeditorPattern initWithPattern:@"\\b(abstract|boolean|break|byte|case|catch|char|class|continue|default|do|double|else|extends|false|final|finally|float|for|if|implements|import|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|super|seitch|synchronized|this|throw|throws|transient|try|true|void|volatile|while)\\b"]
+                                            ]
+                                  keyword:@[
+                                            [CodeditorPattern initWithPattern:@"\\b([a-zA-Z0-9_]+?)\\(" rightOffset:1]
+                                            ]
+                    ];
+            
+        case CodeditorLanguagePascal:
+            return [self initWithLanguage:CodeditorLanguagePascal
+                     codeBlockBeginSymbol:@"begin"
+                       codeBlockEndSymbol:@"end"
+                       withPatternComment:@[
+                                            [CodeditorPattern initWithPattern:@"//(.*)"],
+                                            [CodeditorPattern initWithPattern:@"\\{(.*?)(\\}|$)" globalMatch:YES]
+                                            ]
+                                  grammar:@[
+                                            [CodeditorPattern initWithPattern:@"\\b(absolute|abstract|and|array|as|asm|assembler|at|automated|begin|case|cdecl|class|const|constructor|contains|default|destructor|dispid|dispinterface|div|do|downto|dynamic|else|end|except|export|exports|external|far|file|finalization|finally|for|forward|function|goto|if|implementation|implements|in|index|inherited|initialization|inline|interface|is|label|library|message|mod|name|near|nil|nodefault|not|object|of|on|or|out|overload|override|package|packed|pascal|private|procedure|program|property|protected|public|published|raise|read|readonly|record|register|reintroduce|repeat|requires|resident|resourcestring|safecall|set|shl|shr|stdcall|stored|string|then|threadvar|to|try|type|unit|until|uses|var|virtual|while|with|write|writeonly|xor)\\b"]
                                             ]
                                   keyword:@[
                                             [CodeditorPattern initWithPattern:@"\\b([a-zA-Z0-9_]+?)\\(" rightOffset:1]
