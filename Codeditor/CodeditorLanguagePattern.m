cfaf6437fc29ecf8331f2d42943a353635db7049
GuessEver <guessever@gmail.com>
2016-08-19 08:33:45 +0800

    pattern added to language cpp

--- Codeditor/CodeditorLanguagePattern.m
+++ Codeditor/CodeditorLanguagePattern.m
@@ -141,7 +141,7 @@
                                             [CodeditorPattern initWithPattern:@"/\\*(.*?)(\\*/|$)" globalMatch:YES]
                                             ]
                                   grammar:@[
-                                            [CodeditorPattern initWithPattern:@"\\b(asm|auto|bool|break|case|catch|char|class|const|const_cast|continue|default|delete|do|double|dynamic_cast|else|enum|explicit|export|extern|false|float|for|friend|goto|if|inline|int|long|mutable|namespace|new|operator|private|protected |public|register|reinterpret_cast|return|short|signed|sizeof|static|static_cast|struct|switch|template|this|throw|true|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|while)\\b"]
+                                            [CodeditorPattern initWithPattern:@"\\b(asm|auto|bool|break|case|catch|char|class|const|const_cast|continue|default|delete|do|double|dynamic_cast|else|enum|explicit|export|extern|false|float|for|friend|goto|if|inline|int|long|mutable|namespace|new|operator|private|protected|public|register|reinterpret_cast|return|short|signed|sizeof|static|static_cast|struct|switch|template|this|throw|true|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|while|include)\\b"]
                                             ]
                                   keyword:@[
                                             [CodeditorPattern initWithPattern:@"\\b([a-zA-Z0-9_]+?)\\(" rightOffset:1]
